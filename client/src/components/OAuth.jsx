0145c51011707c4e45fa3eca9f3523ff4cfd6122
muthu0138 <mk0330158@gmail.com>
2024-09-26 17:37:25 +0530

    Added Google OAuth Functionality to Signin and Signup page.

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,49 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+import {AiFillGoogleCircle} from 'react-icons/ai'
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {app} from '../firebase.js'
+import { signinSuccess } from '../redux/user/userSlice.js'
+import {useDispatch} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function OAuth() {
+    const navigate = useNavigate();
+    const auth = getAuth(app)
+    const dispatch = useDispatch()
+    const handleGoogleClick = async () =>{
+        const provider = new GoogleAuthProvider()
+        provider.setCustomParameters({prompt: 'select_account'})
+        try{
+            const resultsFromGoogle = await signInWithPopup(auth , provider)
+            const res = await fetch ('api/auth/google' , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: resultsFromGoogle.user.displayName,
+                    email: resultsFromGoogle.user.email,
+                    googlePhotoUrl: resultsFromGoogle.user.photoURL,
+
+                }
+                )
+            })
+            const data = await res.json()
+            if(res.ok){
+                dispatch(signinSuccess(data))
+                navigate("/");
+            }
+            
+        }catch(err){
+            console.log(err);
+            
+        }
+    }
+    return (
+    <Button type='button' gradientDuoTone="pinkToOrange" outline onClick={handleGoogleClick}>
+       <AiFillGoogleCircle  className='h-6 w-6 mr-2'/>
+        Continue With Google
+    </Button>
+  )
+}
---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-39795.firebaseapp.com",
+  projectId: "mern-blog-39795",
+  storageBucket: "mern-blog-39795.appspot.com",
+  messagingSenderId: "107589397847",
+  appId: "1:107589397847:web:eccbb36a65c90d56952212",
+  measurementId: "G-X82ELMSE02"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
