c9cba7efb30c1c1181e3f2a1e91fb2178fb227d1
muthu0138 <mk0330158@gmail.com>
2024-09-28 20:02:41 +0530

    now the user data can be updated in header

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -16,6 +16,7 @@ export default function OAuth() {
         provider.setCustomParameters({prompt: 'select_account'})
         try{
             const resultsFromGoogle = await signInWithPopup(auth , provider)
+            //console.log(resultsFromGoogle)
             const res = await fetch ('api/auth/google' , {
                 method: 'POST',
                 headers: {
@@ -30,6 +31,7 @@ export default function OAuth() {
                 )
             })
             const data = await res.json()
+            //console.log(data);
             if(res.ok){
                 dispatch(signinSuccess(data))
                 navigate("/");
