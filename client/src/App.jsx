e5fd77852e1903ee5aba24c13ce20b17f29796f8
muthu0138 <mk0330158@gmail.com>
2024-10-08 21:23:43 +0530

    create post ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../src/pages/Home.jsx'
-import About from '../src/pages/About.jsx'
-import Dashboard from '../src/pages/Dashboard.jsx'
-import Projects from '../src/pages/Projects.jsx'
-import Signin from '../src/pages/Signin.jsx'
-import Signup from '../src/pages/Signup.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home.jsx";
+import About from "../src/pages/About.jsx";
+import Dashboard from "../src/pages/Dashboard.jsx";
+import Projects from "../src/pages/Projects.jsx";
+import Signin from "../src/pages/Signin.jsx";
+import Signup from "../src/pages/Signup.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute.jsx';
+import CreatePost from './pages/CreatePost.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/sign-in' element={<Signin/>} />
-          <Route path='/sign-up' element={<Signup/>} />
-          <Route element={<PrivateRoute/>}>
-             <Route path='/dashboard' element={<Dashboard/>} />
-          </Route>
-          <Route path='/projects' element={<Projects/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
