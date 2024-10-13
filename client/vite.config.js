9aeede31b0d9f2464322c2be24dbafa0b8de3ed1
muthu0138 <mk0330158@gmail.com>
2024-10-13 23:16:53 +0530

    pointless commits

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    build: {
-      outDir: 'dist', // Output directory for the build
-    },
     proxy:{
       '/api': {
         target: 'http://localhost:3000',
